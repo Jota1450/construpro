2edaf59ead1b3fce3f7dcbd45b37835a556eb085
Magg0929 <55568384+Magg0929@users.noreply.github.com>
2022-05-11 23:17:53 -0500

    Commit #2
    Implementación js de home y vista project create

--- src/app/components/card-project/card-project.component.ts
+++ src/app/components/card-project/card-project.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pro-card-project',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProCardProject implements OnInit {
 
+  @Input() title ='';
+  @Input() address ='';
+  @Input() startDate ='';
+  @Input() endDate ='';
+  @Input() contractNumber ='';
+  @Input() contracts = [];
+  @Input() id = '';
+  
+
   constructor() { }
 
   ngOnInit() {}
+
 
 }
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,33 @@ import { ComponentsModule } from '../components/components.module';
 })
 export class HomePage {
 
+  contracts=[
+    {title: 'Casa 5',
+     address: 'Calle 5 #5-5',
+     startDate:'20-05-2022',
+     endDate: '25-05-2022',
+     contractNumber: '1',
+     id: '1' 
+
+    },
+    {title: 'Casa 6',
+    address: 'Calle 5 #5-5',
+    startDate:'20-05-2022',
+    endDate: '25-05-2022',
+    contractNumber: '2',
+    id: '2'
+
+    },
+    {title: 'Torre 6',
+    address: 'Calle 7 #7-8',
+    startDate:'20-05-2022',
+    endDate: '25-05-2022',
+    contractNumber: '3',
+    id: '3'
+    }
+
+  ]
+
   constructor() {}
 
 }
