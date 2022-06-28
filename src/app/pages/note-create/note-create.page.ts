053b96ec2a8138ef85a56c70a192a71c96c46373
Jorge Torres <jorge.torres@teky.com>
2022-06-28 13:15:33 -0500

    Update note-create.page.ts

--- src/app/pages/note-create/note-create.page.ts
+++ src/app/pages/note-create/note-create.page.ts
@@ -25,9 +25,9 @@ export class NoteCreatePage implements OnInit {
   users: User[];
   roles: Rol[];
   images: any[] = [
-    'https://larazon.co/wp-content/uploads/2021/11/WhatsApp-Image-2021-11-25-at-11.42.49-AM.jpeg',
-    'https://i.pinimg.com/originals/26/26/0d/26260d6850d544d5d488bfe64f84ef38.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/8/81/Bufo_bufo_03.jpg'
+    //'https://larazon.co/wp-content/uploads/2021/11/WhatsApp-Image-2021-11-25-at-11.42.49-AM.jpeg',
+    //'https://i.pinimg.com/originals/26/26/0d/26260d6850d544d5d488bfe64f84ef38.jpg',
+    //'https://upload.wikimedia.org/wikipedia/commons/8/81/Bufo_bufo_03.jpg'
   ];
   imageUrls: any[] = [];
   user: User;
